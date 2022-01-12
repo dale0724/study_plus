317cad1d2591412fa65c82a99e0e874f7f25015e
imJenny614 <yezhen974@gmail.com>
2022-01-12 23:27:18 +0800

    add and remove function added

--- components/indexSwappingQueryBox.js
+++ components/indexSwappingQueryBox.js
@@ -1,14 +1,21 @@
 import styles from "../styles/AnouncementBox.module.css";
 import {Card, Row} from "react-bootstrap";
 import * as React from "react";
+import {Fragment, useState} from "react";
 
 export default class IndexSwappingQueryBox extends React.Component {
+
     constructor(props) {
         super(props);
-        this.state = {courseCode: '', currentIndex:'', wantedIndex:''};
-
+        this.state = {courseCode: '', currentIndex:'', wantedIndexList:[{
+                type: "text",
+                id: 0,
+                value: ""
+            }]};
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
     }
 
     handleChange(event) {
@@ -19,13 +26,32 @@ export default class IndexSwappingQueryBox extends React.Component {
             this.setState({currentIndex: event.target.value});
         }
         else if (event.target.name=="wantedIndex") {
-            this.setState({wantedIndex: event.target.value});
+            const index = event.target.id;
+            const newList = this.state.wantedIndexList.slice()
+            newList[index].value = event.target.value;
+            this.setState({wantedIndexList: newList});
         }
     }
 
     handleSubmit(event) {
         alert('An index swapping request was submitted,  course code: ' + this.state.courseCode + ', current index: '+ this.state.currentIndex + ', wanted index: '+ this.state.wantedIndex+ '. Good Luck!');
         event.preventDefault();
+    }
+
+    handleAdd(event) {
+        event.preventDefault();
+        this.setState({wantedIndexList: [...this.state.wantedIndexList, {
+            type: "text",
+            id: this.state.wantedIndexList.length,
+            value: ""
+        }]});
+    }
+
+    handleRemove(event) {
+        event.preventDefault();
+        if (this.state.wantedIndexList.length >1){
+            this.setState({wantedIndexList: [...this.state.wantedIndexList.slice(0, this.state.wantedIndexList.length-1)]});
+        }
     }
 
     render() {
@@ -45,10 +71,21 @@ export default class IndexSwappingQueryBox extends React.Component {
                         Current Index:
                         <input type="text" name= "currentIndex" value={this.state.currentIndex} onChange={this.handleChange} />
                     </label><br/>
-                    <label>
-                        Wanted Index 1:
-                        <input type="text" name= "wantedIndex" value={this.state.wantedIndex} onChange={this.handleChange} />
-                    </label><br/>
+                    {this.state.wantedIndexList.map((item, i) => {
+                        return (
+                            <div>
+                            <label>Wanted Index {i+1}:
+                            <input
+                                type="text"
+                                name= "wantedIndex"
+                                value={item.value}
+                                onChange={this.handleChange}
+                                id={i}
+                            /></label></div>
+                        );
+                    })}
+                    <input type="button" onClick={this.handleAdd} value="+" style={{display:"inline-block", margin:"1em"}}/>
+                    <input type="button" onClick={this.handleRemove} value="-"style={{display:"inline-block"}}/><br/>
                     <input type="submit" value="Create a new request!" />
                 </form>
             </div>
