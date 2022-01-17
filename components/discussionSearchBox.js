00d0b537d13a80a0d89251c5a35b267899dc590b
imJenny614 <yezhen974@gmail.com>
2022-01-18 01:37:49 +0800

    finish search box

--- components/discussionSearchBox.js
+++ components/discussionSearchBox.js
@@ -1,5 +1,5 @@
 import styles from "../styles/discussionSearchBox.module.css";
-import {Row} from "react-bootstrap";
+import {Col, Row} from "react-bootstrap";
 import * as React from "react";
 import Button from 'react-bootstrap/Button';
 import CrossSvg from '../public/cross.svg';
@@ -10,8 +10,8 @@ export default class DiscussionSearchBox extends React.Component {
         this.state = {searchText: '', addedTagList:[{
                 type: "text",
                 id: 0,
-                value: ""
-            }], searchTagText:''};
+                value: "EE4717"
+            }], searchTagText:'', id:"0"};
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleTagAdd = this.handleTagAdd.bind(this);
@@ -20,37 +20,42 @@ export default class DiscussionSearchBox extends React.Component {
     }
 
     handleChange(event) {
+        if (event.target.name=="searchText"){
+            this.setState({searchText: event.target.value});
+        }else if (event.target.name=="searchTagText"){
+            this.setState({searchTagText: event.target.value});
+        }
 
     }
 
     handleSubmit(event) {
-        let indexList = []
-        this.state.wantedIndexList.forEach(function (arrayItem) {
-            indexList.push(arrayItem.value);
-        });
-        if (this.state.courseCode!=""&this.state.currentIndex!=""&indexList.toString()!=""){
-            alert('An index swapping request was submitted,  course code: ' + this.state.courseCode + ', current index: '+ this.state.currentIndex + ', wanted index: '+ indexList.toString() + '. Good Luck!');
-        }
-        else {
-            alert("You need to input enough information to create a request!");
-        }
+        /*connect to DB and search for matched result*/
         event.preventDefault();
     }
 
     handleTagAdd(event) {
-        event.preventDefault();
-        this.setState({wantedIndexList: [...this.state.wantedIndexList, {
+        const newID = parseInt(this.state.id)+1;
+        const newValue = event.currentTarget.value
+        this.setState((prevState) => ({
+            addedTagList: [...prevState.addedTagList, {
                 type: "text",
-                id: this.state.wantedIndexList.length,
-                value: ""
-            }]});
+                id: parseInt(prevState.id)+1,
+                value: newValue
+            }],
+            id: parseInt(prevState.id)+1
+        }));
+        console.log(this.state.addedTagList, newID)
+        event.preventDefault();
     }
 
     handleTagRemove(event) {
+        const toDeleteID = parseInt(event.currentTarget.id)
+        const before = [...this.state.addedTagList]
+        const after = before.filter(function(obj) {
+            return parseInt(obj.id) !== toDeleteID;
+        });
+        this.setState({addedTagList: after});
         event.preventDefault();
-        if (this.state.wantedIndexList.length >1){
-            this.setState({wantedIndexList: [...this.state.wantedIndexList.slice(0, this.state.wantedIndexList.length-1)]});
-        }
     }
 
     handleTagSearch(event) {
@@ -62,7 +67,7 @@ export default class DiscussionSearchBox extends React.Component {
             <div className={`mt-3 ${styles.border}`} >
                 <form onSubmit={this.handleSubmit}>
                     <Row>
-                        <input type="text" name="searchText" style={{width:"50%", borderRadius:"0.25rem"}} value={this.state.searchText} onChange={this.handleChange} />
+                        <input type="text" name="searchText" style={{width:"50%", borderRadius:"0.25rem"}} value={this.state.searchText} onChange={this.handleChange} placeholder={"search posts..."}/>
                         <Button onClick={this.handleSubmit} style={{width:"30%", marginLeft:"1rem", display:"inline-block", background:"#7BA1C7"}}>Search</Button>
                     </Row>
                     <br/>
@@ -71,27 +76,63 @@ export default class DiscussionSearchBox extends React.Component {
                     </Row>
                     <Row>
                         <div className={styles.tagsContainer}>
-                            <Button style={{backgroundColor:"#FFFFFF", borderColor: "#000000", padding:"1px"}}>
+                            {this.state.addedTagList.map((item) => {
+                                return (
+                                    <React.Fragment key={item.id}>
+                                        <Button  id={item.id} style={{backgroundColor:"#7BA1C7", borderColor: "#000000", padding: "2px", marginRight: "0.25rem"}} onClick={this.handleTagRemove}>
+                                            <div>
+                                                <div className={styles.leftPanel}>
+                                                    <span style={{color:"#FFFFFF", alignSelf:"center"}}>{item.value}</span>
+                                                </div>
+                                                <div className={styles.rightPanel}>
+                                                    <CrossSvg fill="#FFFFFF"/>
+                                                </div>
+                                            </div>
+                                        </Button>
+                                    </React.Fragment>
+                                );
+                            })}
+                        </div>
+                    </Row>
+                    <br/>
+                    <Row>
+                        <input type="text" name="searchTagText" style={{width:"50%", borderRadius:"0.25rem"}} value={this.state.searchTagText} onChange={this.handleTagSearch} placeholder={"search for tags..."}/>
+                    </Row>
+                    <Row>
+                        <div className={styles.searchTagsResultContainer}>
+                            <Button value="javascript" style={{backgroundColor:"rgb(245, 245, 245)", borderColor: "#000000", padding:"2px", marginRight:"0.25rem"}} onClick={this.handleTagAdd}>
                                 <div>
                                     <div className={styles.leftPanel}>
-                                        <span style={{color:"#000000", fontSize:"15px", alignSelf:"center"}}>EE4717</span>
+                                        <span style={{color:"#000000", fontSize:"15px", alignSelf:"center"}}>javascript</span>
                                     </div>
                                     <div className={styles.rightPanel}>
-                                        <CrossSvg fill="#7BA1C7" className={styles.crossIcon}/>
+                                        <span style={{color:"#000000", fontSize:"15px", alignSelf:"center"}}>20</span>
+                                    </div>
+                                </div>
+                            </Button>
+                            <Button value="PHP" style={{backgroundColor:"rgb(245, 245, 245)", borderColor: "#000000", padding:"2px", marginRight:"0.25rem"}} onClick={this.handleTagAdd}>
+                                <div>
+                                    <div className={styles.leftPanel}>
+                                        <span style={{color:"#000000", fontSize:"15px", alignSelf:"center"}}>PHP</span>
+                                    </div>
+                                    <div className={styles.rightPanel}>
+                                        <span style={{color:"#000000", fontSize:"15px", alignSelf:"center"}}>18</span>
+                                    </div>
+                                </div>
+                            </Button>
+                            <Button value="Mysql DB" style={{backgroundColor:"rgb(245, 245, 245)", borderColor: "#000000", padding:"2px", marginRight:"0.25rem"}} onClick={this.handleTagAdd}>
+                                <div>
+                                    <div className={styles.leftPanel}>
+                                        <span style={{color:"#000000", fontSize:"15px", alignSelf:"center"}}>Mysql DB</span>
+                                    </div>
+                                    <div className={styles.rightPanel}>
+                                        <span style={{color:"#000000", fontSize:"15px", alignSelf:"center"}}>25</span>
                                     </div>
                                 </div>
                             </Button>
                         </div>
                     </Row>
-                    <Row>
-                        <label>
-                            <input type="text" name= "currentIndex" value={this.state.addedTagList} onChange={this.handleChange} />
-                        </label>
-                    </Row>
-                    <Button onClick={this.handleAdd} style={{display:"inline-block", margin:"1em", background:"#7BA1C7"}}>+</Button>
-                    <Button onClick={this.handleRemove} style={{display:"inline-block", margin:"1em", background:"#7BA1C7"}}>-</Button><br/>
-                    <Button as="input" type="submit" value="Create a new request!" style={{margin:"1em", background:"#7BA1C7"}}/>
-                </form>
+                    </form>
             </div>
         );
     }
