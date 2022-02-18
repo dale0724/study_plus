b74e278bdd12c89e0ef5b82ab8fb0a9f32a27dc1
dale0724 <42413229+dale0724@users.noreply.github.com>
2022-02-18 23:10:47 +0800

    register Serviece worker and subscribe

--- pages/_app.js
+++ pages/_app.js
@@ -3,31 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import TimeAgo from 'javascript-time-ago'
 import en_SG from 'javascript-time-ago/locale/en-SG.json'
 import { useEffect } from "react"
+import { fetchWrapper } from '../tools/fetchWrapper'
+import { API_url } from '../app_config'
+import { useLoggedUserData } from "../tools/helper";
 
 TimeAgo.addDefaultLocale(en_SG)
 
-
 function MyApp({ Component, pageProps }) {
 
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", function () {
-        navigator.serviceWorker.register("/sw.js").then(
-          function (registration) {
-            console.log("Service Worker registration successful with scope: ", registration.scope);
-          },
-          function (err) {
-            console.log("Service Worker registration failed: ", err);
-          }
-        );
-      });
-    }
-  }, [])
-  useEffect(()=>{
-    if(Notification.permission != 'granted'){
-      Notification.requestPermission(()=>{})
-    }
-  })
   return (
     <>
       <Component {...pageProps} />
