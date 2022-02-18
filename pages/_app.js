9d5278b79e3c44980f24d69576e3a97e1a58e1c0
dale0724 <42413229+dale0724@users.noreply.github.com>
2022-02-18 18:22:56 +0800

    added sw.js and ask for notification permission

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,37 @@ import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import TimeAgo from 'javascript-time-ago'
 import en_SG from 'javascript-time-ago/locale/en-SG.json'
+import { useEffect } from "react"
 
 TimeAgo.addDefaultLocale(en_SG)
 
 
 function MyApp({ Component, pageProps }) {
-  return(
+
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
+  useEffect(()=>{
+    if(Notification.permission != 'granted'){
+      Notification.requestPermission(()=>{})
+    }
+  })
+  return (
     <>
-    <Component {...pageProps} />
+      <Component {...pageProps} />
     </>
-    
-  ) 
-  
+  )
 }
 
 export default MyApp
--- pages/test.js
+++ pages/test.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Row } from "react-bootstrap";
 import DiscussionAddModal from "../components/discussion/discussionAddModal";
 import MediaEditor from "../components/discussion/mediaEditor";
 import MediaEditorExample from "../components/discussion/mediaEditorExample";
@@ -7,20 +7,28 @@ import MyForm from "../components/helpers/myForm";
 import TodoModal from "../components/todo/todoModal";
 
 export default function Test() {
-    const [showAddModal, setShowAddModal] = useState(false)
-    function handleAddModalClose(){
-        setShowAddModal(false)
+    const [fillColor, setFillColor] = useState("none")
+    function handleSVGClick() {
+        if (fillColor=="none"){
+            setFillColor('#7BA1C7')
+        }else{
+            setFillColor('none')
+        }
     }
-    function handleAddModalShow(){
-        setShowAddModal(true)
-    }
-
-
     return (
         <Container className="text-center">
-            <DiscussionAddModal show={showAddModal} handleClose={handleAddModalClose}/>
-            <Button className="mt-3" onClick={handleAddModalShow }>Click Me! </Button>
-            <MediaEditor></MediaEditor>
+             <Row>
+                                            <div onClick={handleSVGClick}>
+                                            <svg id="upVote" width="30" height="30" viewBox="0 0 24 24" fill={fillColor}
+                                                 xmlns="http://www.w3.org/2000/svg">
+                                                <path
+                                                    d="M7.57256 20.9999H4.82902C4.34394 20.9999 3.87872 20.8102 3.53571 20.4727C3.1927 20.1351 3 19.6773 3 19.1999V12.8999C3 12.4226 3.1927 11.9647 3.53571 11.6272C3.87872 11.2896 4.34394 11.0999 4.82902 11.0999H7.57256M13.9741 9.29996V5.69998C13.9741 4.9839 13.6851 4.29715 13.1706 3.79081C12.6561 3.28446 11.9582 3 11.2306 3L7.57256 11.0999V20.9999H17.8882C18.3293 21.0048 18.7574 20.8527 19.0935 20.5715C19.4296 20.2903 19.6511 19.8991 19.7173 19.4699L20.9793 11.3699C21.0191 11.112 21.0014 10.8486 20.9275 10.598C20.8535 10.3474 20.7251 10.1156 20.5512 9.91872C20.3772 9.72182 20.1618 9.5645 19.9199 9.45765C19.678 9.35081 19.4154 9.29701 19.1503 9.29996H13.9741Z"
+                                                    stroke="black" strokeWidth="1.5" strokeLinecap="round"
+                                                    strokeLinejoin="round"/>
+                                            </svg>
+                                            </div>
+                                        </Row>
+            {/* <Button className="mt-3" onClick={handleClick}>Click Me! </Button> */}
         </Container>
     )
 }
