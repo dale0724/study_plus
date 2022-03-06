374bfc795c9ecfbd44bee5681708bc1477569944
dale0724 <42413229+dale0724@users.noreply.github.com>
2022-03-06 15:14:38 +0800

    refactored

--- components/indexSwapping/indexSwappingRequestBox.js
+++ components/indexSwapping/indexSwappingRequestBox.js
@@ -1,33 +1,41 @@
 import styles from "../../styles/indexSwappingRequestBox.module.css";
-import {Card, ListGroup, ListGroupItem, Row} from "react-bootstrap";
-import React, {useState} from "react";
-import {API_url} from "../../app_config";
+import { Card, ListGroup, ListGroupItem, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { API_url } from "../../app_config";
 import IndexSwappingCard from "./indexSwappingCard";
 import useSWR from "swr";
 import MySpinner from "../mySpinner";
+import { IndexSwappingClient } from "../../api_client/index_swapping/client";
+
 
 export default function IndexSwappingRequestBox() {
     const [displayState, SetState] = useState('all');
     const fetcher = (...args) => fetch(...args).then((res) => res.json())
-    const {data: user} = useSWR('/api/auth', fetcher);
+    const { data: user } = useSWR('/api/auth', fetcher);
     var boxContent = ''
-    if (displayState=='all'){
-        const {data:allRequestData, error: allRequestErr} = useSWR(API_url.get_all_index_swapping_posts, fetcher)
+    if (displayState == 'all') {
+        const { data: allRequestData, isLoading, isError: allRequestErr } = IndexSwappingClient.useAllIndexSwappingPosts()
+        // const {data:allRequestData, error: allRequestErr} = useSWR(API_url.get_all_index_swapping_posts, fetcher)
         if (allRequestErr) {
             boxContent = "Error"
         } else {
-            if (allRequestData) {
-                const postMetaDataList = allRequestData['data'].map(jsonData => JSON.parse(jsonData))
-                boxContent = postMetaDataList.map(postMetaData =>
-                    <ListGroupItem key={postMetaData.id}>
-                        <IndexSwappingCard metaData={postMetaData}/>
-                    </ListGroupItem>)
+            if (!isLoading) {
+                // const postMetaDataList = allRequestData['data'].map(jsonData => JSON.parse(jsonData))
+                // boxContent = postMetaDataList.map(postMetaData =>
+                //     <ListGroupItem key={postMetaData.id}>
+                //         <IndexSwappingCard metaData={postMetaData}/>
+                //     </ListGroupItem>)
+                boxContent = allRequestData.map(post =>
+                    <ListGroupItem key={ post.id }>
+                        <IndexSwappingCard metaData={post} />
+                    </ListGroupItem>
+                 )
             } else {
                 boxContent = <MySpinner></MySpinner>
             }
         }
-    }else if (displayState=='my') {
-        const {data:myRequestData, error: myRequestErr} = useSWR(() =>API_url.get_my_index_swapping_posts_by_email+user.email, fetcher)
+    } else if (displayState == 'my') {
+        const { data: myRequestData, error: myRequestErr } = useSWR(() => API_url.get_my_index_swapping_posts_by_email + user.email, fetcher)
         if (myRequestErr) {
             boxContent = "Error"
         } else {
@@ -35,15 +43,15 @@ export default function IndexSwappingRequestBox() {
                 const postMetaDataList = myRequestData['data'].map(jsonData => JSON.parse(jsonData))
                 boxContent = postMetaDataList.map(postMetaData =>
                     <ListGroupItem key={postMetaData.id}>
-                        <IndexSwappingCard metaData={postMetaData}/>
+                        <IndexSwappingCard metaData={postMetaData} />
                     </ListGroupItem>)
             } else {
                 boxContent = <MySpinner></MySpinner>
             }
         }
     }
-    else if (displayState=='matched'){
-        const {data:matchedRequestData, error: matchedRequestErr} = useSWR(() =>API_url.get_matched_index_swapping_posts_by_email+user.email, fetcher)
+    else if (displayState == 'matched') {
+        const { data: matchedRequestData, error: matchedRequestErr } = useSWR(() => API_url.get_matched_index_swapping_posts_by_email + user.email, fetcher)
         if (matchedRequestErr) {
             boxContent = "Error"
         } else {
@@ -51,7 +59,7 @@ export default function IndexSwappingRequestBox() {
                 const postMetaDataList = matchedRequestData['data'].map(jsonData => JSON.parse(jsonData))
                 boxContent = postMetaDataList.map(postMetaData =>
                     <ListGroupItem key={postMetaData.id}>
-                        <IndexSwappingCard metaData={postMetaData}/>
+                        <IndexSwappingCard metaData={postMetaData} />
                     </ListGroupItem>)
             } else {
                 boxContent = <MySpinner></MySpinner>
@@ -61,18 +69,18 @@ export default function IndexSwappingRequestBox() {
 
     return (
         <>
-            <div className={`mt-3 ${styles.border}`} style={{height: '70vh'}}>
+            <div className={`mt-3 ${styles.border}`} style={{ height: '70vh' }}>
                 <Row className="m-0">
                     <Card border="light" className={styles.titleCard}>
-                        <a className={styles.titleText} style={{ cursor: "pointer" }} onClick={()=>{SetState('all')}}>All Pending Requests</a>
+                        <a className={styles.titleText} style={{ cursor: "pointer" }} onClick={() => { SetState('all') }}>All Pending Requests</a>
                         <span className={styles.titleText}>{''}|{''}</span>
-                        <a className={styles.titleText} style={{ cursor: "pointer" }} onClick={()=>{SetState('my')}}>My Requests</a>
+                        <a className={styles.titleText} style={{ cursor: "pointer" }} onClick={() => { SetState('my') }}>My Requests</a>
                         <span className={styles.titleText}>{''}|{''}</span>
-                        <a className={styles.titleText} style={{ cursor: "pointer" }} onClick={()=>{SetState('matched')}}>All Pending
+                        <a className={styles.titleText} style={{ cursor: "pointer" }} onClick={() => { SetState('matched') }}>All Pending
                             Requests</a>
                     </Card>
                 </Row>
-                <ListGroup style={{overflow: 'hidden auto', height: '90%', width: '95%', margin: 'auto'}}>
+                <ListGroup style={{ overflow: 'hidden auto', height: '90%', width: '95%', margin: 'auto' }}>
                     {boxContent}
                 </ListGroup>
             </div>
--- pages/test.js
+++ pages/test.js
@@ -9,14 +9,25 @@ import TodoModal from "../components/todo/todoModal";
 import { Editor, EditorState, RichUtils, convertToRaw, convertFromRaw } from 'draft-js';
 import { fetcher, fetchWrapper } from "../tools/fetchWrapper";
 import useSWR from "swr";
+import IndexSwappingDTO from "../DTO/index_swapping"
+import {IndexSwappingClient} from "../api_client/index_swapping/client";
 
 export default function Test() {
-    
-    const {data, error} = useSWR("/api/hello", fetcher)
+    const indexSwappingClient = new IndexSwappingClient
+    const {data} = indexSwappingClient.useHello()
+    // const {data, error} = IndexSwappingClient.useHello()
+    // const {data, error} = useSWR("/api/hello", fetcher)
     console.log(data)
-    if(error){
-        console.log(error)
+
+    const test_object = {
+        course_title: "test_add",
+        current_index: "2",
+        wanted_indexes: ['1'],
+        content: "Hi",
+        user_email: "lihang0722@gmail.com"
     }
+    const instance = IndexSwappingDTO.ObjectToInstance(test_object)
+    console.log(instance)
     return (
         <Container className="text-center">
                 <h1>
