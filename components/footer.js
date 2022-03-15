85589369cacd80e898bb84346aff13923daf554d
dale <lihang0724@gmail.com>
2022-03-15 19:42:20 +0800

    optimised footer

--- components/footer.js
+++ components/footer.js
@@ -3,9 +3,10 @@
 export default function Footer() {
     return (
         <>
-            <footer className="text-center bottom-0 border-top mt-3" >
-                <p>Contributor: Li Hang, Ye Qianhan</p>
-                <p><a href="mailto:lihang0724@gmail.com">Contact Us via Email</a></p>
+            <footer className="text-center bottom-0 position-absolute" style={{height:"95px", width: '100%', borderTop: '1px solid #dedede'}}>
+                <br/>
+                <p className="m-0">Contributor: Li Hang, Ye Qianhan</p>
+                <p className="m-0"><a href="mailto:lihang0724@gmail.com">Contact Us via Email</a></p>
             </footer>
         </>
     )
